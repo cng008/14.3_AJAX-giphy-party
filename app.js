0776218237c28f608d0cd3655412089d0d8a99fa
cng008 <92824407cng008@users.noreply.github.com>
2022-01-08 12:23:22 -0800

    working functionality with catch error

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-console.log("Let's get this party started!");
+const results = document.getElementById('results');
+/* use ajax result to add a gif */
+async function addGif(term) {
+  const res = await axios.get('https://api.giphy.com/v1/gifs/search', {
+    params: {
+      q: term,
+      api_key: '1hWyguDasbRNDfI7r0gL48WV3GP5EzU0'
+    }
+  });
+  const obj = res.data.data;
+  const randomKey = obj[randomProperty(obj)];
 
-const apiKey = '1hWyguDasbRNDfI7r0gL48WV3GP5EzU0';
-
-//prettier-ignore
-async function getGif(term, api) {
-  const res = await axios.get('https://api.giphy.com/v1/gifs/search', { params: { q: term, api_key: api }});
-  // const res = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchItem}&api_key=${apiKey}`);
-  console.log(res);
+  // APPEND GIF TO RESULTS SECTION
+  try {
+    const newGif = document.createElement('img');
+    newGif.classList.add('rounded', 'm-2');
+    newGif.setAttribute('height', '200px');
+    newGif.src = randomKey.images.original.url; //returns URL from random array
+    results.append(newGif);
+  } catch (error) {
+    alert("that's not a word!");
+  }
 }
 
-// PICK RANDOM GIF FROM ARRAY
-
-// GET URL FROM RANDOM ARRAY
+// PICK RANDOM KEY FROM OBJECT ARRAY
+randomProperty = obj => {
+  const objArr = Object.keys(obj); //turns object keys into array
+  return objArr[Math.floor(Math.random() * objArr.length)]; //returns a random number
+};
 
 // SEARCH FOR GIF ON SUBMIT
 const form = document.getElementById('searchForm');
 form.addEventListener('submit', e => {
-  const searchItem = document.getElementById('searched').value;
   e.preventDefault();
-  getGif(searchItem, apiKey);
+  const searchVal = document.getElementById('searched').value;
+  addGif(searchVal);
+});
+
+// REMOVE ALL IMAGES BUTTON
+const removeBtn = document.getElementById('remove');
+removeBtn.addEventListener('click', e => {
+  results.innerHTML = '';
 });
